331ac39381de8f88693c660f2ab91d381d00ccb7
RodrigoLimaM <digo100200@gmail.com>
2024-02-25 10:10:43 -0300

    remving unexpected output

--- src/main/java/br/com/capitalgains/Main.java
+++ src/main/java/br/com/capitalgains/Main.java
@@ -5,7 +5,6 @@ import br.com.capitalgains.adapter.in.CapitalGainsController;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Application Started\n");
         new CapitalGainsController().calculateTaxes();
     }
 }
